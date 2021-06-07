3e012cd34ea6c20a15e0cd678ffb0423065b21c7
Vikas Jha <vikasjhag@gmail.com>
2021-06-07 13:36:17 +0530

    WeatherProvider delete operation implemented

--- app/src/main/java/com/example/data/WeatherProvider.java
+++ app/src/main/java/com/example/data/WeatherProvider.java
@@ -113,7 +113,26 @@ public class WeatherProvider extends ContentProvider {
 
     @Override
     public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
-        return 0;
+        int num_rows_deleted ;
+
+        switch (sUriMatcher.match(uri)){
+            case CODE_WEATHER:
+                if (selection==null){
+                    selection = "1";
+                }
+                num_rows_deleted = mWeatherDbHelper.getWritableDatabase().delete(
+                        WeatherContract.WeatherEntry.TABLE_NAME,
+                        selection,
+                        selectionArgs
+                );
+                if (num_rows_deleted > 0){
+                    getContext().getContentResolver().notifyChange(uri,null);
+                }
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown Uri: "+uri);
+        }
+        return num_rows_deleted;
     }
 
     @Override
