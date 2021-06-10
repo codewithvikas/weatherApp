67dc6b2c99235a087b8b854c380f9a1c078fad2c
Vikas Jha <vikasjhag@gmail.com>
2021-06-10 07:12:44 +0530

    Error : Cannot figure out how to save this field into database. You can consider adding a type converter for it. TypeConverter Added.

--- app/src/main/java/com/example/aac/WeatherDatabase.java
+++ app/src/main/java/com/example/aac/WeatherDatabase.java
@@ -5,8 +5,10 @@ import android.content.Context;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 
 @Database(entities = {WeatherEntity.class},version = 1,exportSchema = false)
+@TypeConverters(DateConverter.class)
 public abstract class WeatherDatabase extends RoomDatabase {
 
     private static final Object LOCK = new Object();
