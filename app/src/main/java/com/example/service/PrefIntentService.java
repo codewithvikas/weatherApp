1cb2607fdf7de489f2000a6aa7702e826dc52284
Vikas Jha <vikasjhag@gmail.com>
2021-06-20 06:15:03 +0530

    Intent service initial code

---
+++ app/src/main/java/com/example/service/PrefIntentService.java
@@ -0,0 +1,23 @@
+package com.example.service;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+import androidx.annotation.Nullable;
+
+
+public class PrefIntentService extends IntentService {
+    /**
+     * Creates an IntentService.  Invoked by your subclass's constructor.
+     *
+     * @param name Used to name the worker thread, important only for debugging.
+     */
+    public PrefIntentService(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+
+    }
+}
