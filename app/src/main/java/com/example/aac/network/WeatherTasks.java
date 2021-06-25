a1e32eb355d5838466f204e0ace8420f49780d8a
Vikas Jha <vikasjhag@gmail.com>
2021-06-25 10:54:46 +0530

    Adding Action Buttons in Notification

--- app/src/main/java/com/example/aac/network/WeatherTasks.java
+++ app/src/main/java/com/example/aac/network/WeatherTasks.java
@@ -6,6 +6,7 @@ import com.example.aac.database.WeatherEntity;
 import com.example.aac.network.WeatherNetworkDataSource;
 import com.example.data.WeatherPreferences;
 import com.example.utils.NetworkUtils;
+import com.example.utils.NotificationUtil;
 import com.example.utils.OpenWeatherJsonUtils;
 import com.example.weatherapp.AppExecutors;
 
@@ -17,14 +18,17 @@ import java.util.List;
 
 public class WeatherTasks {
     public static final String ACTION_UPDATE_LOCATION = "update-weather-location";
+    public static final String ACTION_CLEAR_NOTIFICATION = "clear-notification";
+
 
         public static void executeTask(Context context,String action){
             if (ACTION_UPDATE_LOCATION.equals(action)){
-
                 AppExecutors  appExecutors = AppExecutors.getInstance();
                 WeatherNetworkDataSource weatherNetworkDataSource = WeatherNetworkDataSource.getInstance(context,appExecutors);
                 weatherNetworkDataSource.fetchWeather();
-
+            }
+            else if (ACTION_CLEAR_NOTIFICATION.equals(action)){
+                NotificationUtil.clearAllNotification(context);
             }
         }
 }
